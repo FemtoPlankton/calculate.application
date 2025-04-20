2802fa9a0e416f9cfb70c603b3d18f26862590c4
FemtoPlankton <psx1129@yahoo.co.jp>
2025-04-20 22:37:34 +0900

    20250420Update

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const displayNumber = document.getElementById('display-number');
 const displayOperand = document.getElementById('display-operand');
-const equalButton = document.getElementById('equal-button');
+
 const numberButtons = document.querySelectorAll('.number-button');
 const operatorButtons = document.querySelectorAll('.operator-button');
+const decimalButton = document.querySelector('.decimal-button')
+const equalButton = document.querySelector('.equal-button')
+
 const historyContainer = document.getElementById('history');
 const resetButton = document.getElementById('reset')
 
@@ -10,15 +13,17 @@ let operand = null;
 let previousOperand = "0";
 let pendingOperator = null;
 let shouldResetDisplay = false;
+let equalJudgment = false;
+let resetHistory = false;
 
 function calculate(num1Str, operator, num2Str) {
-    const num1 = parseFloat(displayOperand.textContent);
+    const num1 = parseFloat(num1Str);
     const num2 = parseFloat(num2Str);
 
-    previousOperand = displayOperand.textContent;
+    previousOperand = num1Str;
 
     if (isNaN(num1) || isNaN(num2)) {
-        console.error("無効な数値です:", displayOperand.textContent, operator, num2Str)
+        console.error("無効な数値です:", num1Str, operator, num2Str)
         return "Error";
     }
 
@@ -45,7 +50,11 @@ function calculate(num1Str, operator, num2Str) {
             console.error("無効な演算子です:", operator);
             return "Error";
     }
-    console.log(operator, "で計算。結果:", result);
+
+    const roundedResult = parseFloat(result.toFixed(8));
+    result = roundedResult;
+
+    console.log(operator, "で計算。結果:", roundedResult);
     return result;
 }
 
@@ -53,8 +62,16 @@ numberButtons.forEach(button => {
     button.addEventListener('click', (event) => {
         const clickedNumber = event.target.dataset.value;
 
+        if (equalJudgment) {
+            displayOperand.textContent = "";
+            operand = null;
+            pendingOperator = null;
+            resetHistory = true;
+            equalJudgment = false;
+        }
+
         if (shouldResetDisplay) {
-            displayNumber.textContent = "0";
+            displayNumber.textContent = "";
             shouldResetDisplay = false;
         }
 
@@ -63,13 +80,16 @@ numberButtons.forEach(button => {
             return;
         } else if (displayNumber.textContent === "0" && clickedNumber !== "0") {
             displayNumber.textContent = clickedNumber;
-            operand = displayNumber.textContent;
-            console.log("数値が保持されました。:", clickedNumber)
+            operand = clickedNumber;
             return;
         } else {
-            displayNumber.textContent =  displayNumber.textContent + clickedNumber;
-            operand = displayNumber.textContent;
-            console.log("数値が保持されました。:", clickedNumber)
+            if (equalJudgment) {
+                displayNumber.textContent = clickedNumber;
+                console.log("数値が保持されました。:", clickedNumber);
+            } else {
+                displayNumber.textContent += clickedNumber;
+                console.log("数値が保持されました。:", clickedNumber);
+            }
         }
     });
 });
@@ -78,7 +98,16 @@ operatorButtons.forEach(button => {
     button.addEventListener('click', (event) => {
         const clickedOperator = event.target.dataset.value;
 
-        if (pendingOperator === null && operand === null) {
+        if (equalJudgment) {
+            if (historyDisplay.textContent.includes('finish')) {
+                let currentHistory = historyDisplay.textContent;
+                let cleanedHistory = currentHistory.replace("finish", '');
+                historyDisplay.textContent = cleanedHistory;
+            };
+            equalJudgment = false;
+        }
+
+        if (displayNumber.textContent === null) {
             console.log("数値が保持されていません。");
             return;
         } else if (pendingOperator === null) {
@@ -92,41 +121,110 @@ operatorButtons.forEach(button => {
                 console.log("最初の数値と演算子を保持:", displayNumber.textContent + " ÷");
             } else {
                 displayOperand.textContent = operand + " " + clickedOperator;
-                console.log("最初の数値と演算子を保持:", displayNumber.textContent + " " +clickedOperator);
+                console.log("最初の数値と演算子を保持:", displayNumber.textContent + " " + clickedOperator);
             }
-            displayNumber.textContent = "0";
+            displayNumber.textContent = null;
             return;
         } else {
-            const currentDisplay = displayNumber.textContent;
-            const result = calculate(operand, pendingOperator, currentDisplay);
-            const history = document.querySelectorAll('.history');
+            if (pendingOperator === clickedOperator) {
+                if (displayNumber.textContent === null) {
+                    console.log("数値が保持されていません。")
+                    return;
+                } else {
+                    const currentDisplay = displayOperand.textContent;
+                    const result = calculate(operand, pendingOperator, currentDisplay);
 
-            if (result === "Error") {
-                displayNumber.textContent = "Error";
-                displayOperand.textContent = "";
-                operand = null;
-                pendingOperator = null;
-                shouldResetDisplay = true;
+                    if (result === "Error") {
+                        displayNumber.textContent = "Error";
+                        displayOperand.textContent = "";
+                        operand = null;
+                        pendingOperator = null;
+                        shouldResetDisplay = true;
+                    } else {
+                        let cycleDelete = historyDisplay.textContent.split('\n');
+
+                        if (cycleDelete.length > 15) {
+                            cycleDelete.splice(14, 1);
+                            historyDisplay.textContent = cycleDelete.join('\n');
+                        }
+
+                        if (resetHistory) {
+                            historyDisplay.textContent = '';
+                            resetHistory = false;
+                        }
+
+                        let displayOpSymbol = clickedOperator;
+                        if (clickedOperator === "*") displayOpSymbol = "×";
+                        if (clickedOperator === "/") displayOpSymbol = "÷";
+
+                        historyContainer.style.display = "block";
+                        historyDisplay.textContent = previousOperand + " " + pendingOperator + " " + displayNumber.textContent + " = " + result + "\n" + historyDisplay.textContent;
+
+                        displayOperand.textContent = result + " " + displayOpSymbol;
+                        displayNumber.textContent = result;
+                        operand = result.toString();
+                        pendingOperator = clickedOperator;
+                        shouldResetDisplay = true;
+                        console.log("計算実行:", result, "次の演算子:", pendingOperator);
+                    }
+                }
             } else {
-                let displayOpSymbol = clickedOperator;
-                if (clickedOperator === "*") displayOpSymbol = "×";
-                if (clickedOperator === "/") displayOpSymbol = "÷";
-                
-                historyContainer.style.display = "block";
-                historyDisplay.textContent += previousOperand + " " + currentDisplay + " = " + result + "\n";
+                let replaceOperator = displayOperand.textContent.split(" ");
+                if (displayOperand.textContent.includes(clickedOperator)) {
+                    replaceOperator.splice(1, 1, clickedOperator);
 
-                displayOperand.textContent = result + " " + displayOpSymbol;
-                displayNumber.textContent = result;
-                operand = result.toString();
-                pendingOperator = clickedOperator;
-                shouldResetDisplay = true;
-                console.log("計算実行:", result, "次の演算子:", pendingOperator);
+                    let displayOpSymbol = clickedOperator;
+                    if (clickedOperator === "*") displayOpSymbol = "×";
+                    if (clickedOperator === "/") displayOpSymbol = "÷";
+                }
             }
         };
     });
 });
 
+decimalButton.addEventListener('click', () => {
+    if (displayNumber.textContent.includes(".")) {
+        console.log('既に小数点があります。');
+    } else {
+        displayNumber.textContent += ".";
+    }
+});
+
+equalButton.addEventListener('click', () => {
+    if (equalJudgment) {
+        console.log('既に計算結果が表示されています。')
+    } else {
+        if (operand === null) {
+            console.log("数値が保持されていません。");
+        } else {
+            if (resetHistory) {
+                historyDisplay.textContent = '';
+                resetHistory = false;
+            }
+
+            const currentDisplay = displayNumber.textContent;
+            const result = calculate(operand, pendingOperator, currentDisplay);
+
+            let cycleDelete = historyDisplay.textContent.split('\n');
+
+            if (cycleDelete.length > 15) {
+                cycleDelete.splice(14, 1);
+                historyDisplay.textContent = cycleDelete.join('\n');
+            }
+
+            historyContainer.style.display = "block";
+            historyDisplay.textContent = previousOperand + " " + pendingOperator + " " + displayNumber.textContent + " = " + result + "\n" + historyDisplay.textContent;
+            displayOperand.textContent = null;
+            displayNumber.textContent = result;
+
+            pendingOperator = null;
+            shouldResetDisplay = true;
+            equalJudgment = true;
+        }
+    }
+})
+
 resetButton.addEventListener('click', () => {
     historyDisplay.textContent = '';
     historyContainer.style.display = "none";
-})
+});
